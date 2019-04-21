fb2439f114fd33e5de7eeef793532345c6fefb53
William Brendaw <williambrendaw@protonmail.com>
2019-04-20 23:47:02 -0300

    Add colors in terminal

---
+++ .zsh/01-colors.zsh
@@ -0,0 +1,11 @@
+# William Brendaw's dotfiles
+#
+# author:  brendaw <williambrendaw@protonmail.com>
+# code:    https://github.com/brendaw/dotfiles
+# version: 0.1.0
+#
+# description: defining terminal with colors
+# reference: from this response on stackoverflow -> https://unix.stackexchange.com/a/137340
+
+export CLICOLOR=1
+export LSCOLORS="gxfxcxdxbxegedabagacad"
