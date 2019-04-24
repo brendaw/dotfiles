fc89daee850d1766122c5556e5d2ecaaa411b922
William Brendaw <williambrendaw@protonmail.com>
2019-04-24 00:33:49 -0300

    Add prompt customization

--- .zsh/01-colors.zsh
+++ .zsh/01-colors.zsh
@@ -5,7 +5,25 @@
 # version: 0.1.0
 #
 # description: defining terminal with colors
-# reference: from this response on stackoverflow -> https://unix.stackexchange.com/a/137340
 
+# Defining default colors for ls and the terminal in general, from this response on stackoverflow -> https://unix.stackexchange.com/a/137340
 export CLICOLOR=1
 export LSCOLORS="gxfxcxdxbxegedabagacad"
+
+# Another colors for the terminal
+RED='%F{001}'
+GREEN='%F{002}'
+YELLOW='%F{003}'
+NORMAL='%F{015}'
+GREY='%F{252}'
+
+# Get all the colors spectrum in terminal
+function spectrum_ls() {
+  txt="The quick brown fox jumps over the lazy dog"
+  if (( $# > 0)); then
+    txt=$1
+  fi
+  for code in {000..255}; do
+    print -P -- "$code: %F{$code}$txt%f"
+  done
+}
