124bf1796be7ba5cf694ec71495bbbcaac066be3
William Brendaw <williambrendaw@icloud.com>
2022-11-17 08:23:28 -0300

    Add macOS support on dotfiles install

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-user_directory=/home/$USER
+system_directory=/home
+
+if [ "$(uname)" == "Darwin" ]; then
+    system_directory=/Users
+fi
+
+user_directory=$system_directory/$USER
 
 echo "info: Installing and/or overriding dotfiles into current user directory."
 echo "info: User directory: $user_directory"
 echo "info: Installing..."
 
-yes | cp --recursive .bin .dot .zsh .config .gitconfig .vimrc .zshrc $user_directory
+yes | cp -R .bin .dot .zsh .config .gitconfig .vimrc .zshrc $user_directory
 
 echo "info: Finished intalling dotfiles into $user_directory"
