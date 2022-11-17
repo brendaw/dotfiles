291c221d6841875d11cdf33806e0a34e3fe7bb2f
William Brendaw <williambrendaw@icloud.com>
2022-11-17 08:23:50 -0300

    Improve ssh keys folder config

--- .zsh/00-environment.zsh
+++ .zsh/00-environment.zsh
@@ -24,9 +24,21 @@ export PATH="/usr/local/opt/qt/bin:$PATH"
 # Add custom scripts inside .bin
 export PATH="$PATH:$HOME/.bin"
 
+# Remove glob qualifiers config
+setopt no_bare_glob_qual
+
+ssh_keys_folder=~/.ssh/keys
+
+# Fix keys permissions
+chmod 700 $ssh_keys_folder
+chmod 600 $ssh_keys_folder/*rsa
+chmod 600 $ssh_keys_folder/*ed25519
+chmod 644 $ssh_keys_folder/*.pub
+
 # Add all SSH keys to agent
 eval "$(ssh-agent -s)"
-ssh-add -q ~/.ssh/*
+ssh-add -q $ssh_keys_folder/*ed25519
+ssh-add -q $ssh_keys_folder/*rsa
 
 # Enable all .bin scripts to run
 chmod +x $HOME/.bin/*
