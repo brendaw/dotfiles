a70f3cc2e02e881754e4bcad4ef3beb5bf45df8f
William Brendaw <williambrendaw@icloud.com>
2025-03-22 15:34:31 -0300

    feat(dot): add fix to home and end buttons from macos config

--- .dot/macos.sh
+++ .dot/macos.sh
@@ -14,3 +14,35 @@ pip3 install diagrams
 # Disable recent apps from Dock
 defaults write com.apple.dock show-recents -bool false
 
+# Fix Home and End buttons from External Keyboard
+mkdir -p ~/Library/KeyBindings
+
+cat <<EOF > ~/Library/KeyBindings/DefaultKeyBinding.dict
+{
+    /* Remap Home / End keys */
+
+    /* Home Button*/
+    "\UF729" = "moveToBeginningOfLine:";
+
+    /* End Button */
+    "\UF72B" = "moveToEndOfLine:";
+
+    /* Shift + Home Button */
+    "$\UF729" = "moveToBeginningOfLineAndModifySelection:";
+
+    /* Shift + End Button */
+    "$\UF72B" = "moveToEndOfLineAndModifySelection:";
+
+    /* Ctrl + Home Button */
+    "^\UF729" = "moveToBeginningOfDocument:";
+
+    /* Ctrl + End Button */
+    "^\UF72B" = "moveToEndOfDocument:";
+
+    /* Shift + Ctrl + Home Button */
+    "$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:";
+
+    /* Shift + Ctrl + End Button*/
+    "$^\UF72B" = "moveToEndOfDocumentAndModifySelection:";
+}
+EOF
